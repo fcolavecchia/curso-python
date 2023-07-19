429e5ba8f6ae9106100238aa782f80ad1cb589b7
Flavio Colavecchia <fcolavecchia@gmail.com>
2023-07-19 15:13:51 -0300

    Resorted dirs

--- Assets/Python Code/error.py
+++
@@ -1,9 +0,0 @@
-## module error
-''' err(string).
-    Prints 'string' and terminates program.
-'''    
-import sys
-def err(string):
-    print(string)
-    input('Press return to exit')
-    sys.exit(0)
